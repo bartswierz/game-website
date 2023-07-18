99f16823fe309cdadaec809f6cc78ce75e07c62f
bartswierz <bartoszswierzynski7@gmail.com>
2023-07-18 15:47:09 -0500

    implemented icon and updated design of the Store Page Cards

--- app/browse/stores/page.tsx
+++ app/browse/stores/page.tsx
@@ -1,65 +1,51 @@
 import React from "react";
 import { getStores } from "@/utils";
 import { GameStores } from "@/types";
-import Image from "next/image";
 import Link from "next/link";
 import { FaSteam, FaXbox, FaPlaystation, FaGooglePlay, FaApple } from "react-icons/fa";
 import { SiNintendo, SiEpicgames, SiItchdotio, SiGogdotcom } from "react-icons/si";
-// type Props = {};
 
 const Stores = async () => {
   const storesList: GameStores = await getStores();
   // console.log("storesList: ", storesList);
 
+  // Company Icon List for Stores
+  const iconList = [
+    <FaSteam size={50} />,
+    <FaXbox size={50} />,
+    <FaPlaystation size={50} />,
+    <FaApple size={50} />,
+    <SiGogdotcom size={50} />,
+    <SiNintendo size={50} />,
+    <FaXbox size={50} />,
+    <FaGooglePlay size={50} />,
+    <SiItchdotio size={50} />,
+    <SiEpicgames size={50} />,
+  ];
+
   if (!storesList) return <div className="text-white">Loading...</div>;
 
   return (
-    <div className="text-white">
-      <p>Stores TEST</p>
-      <div className="flex flex-wrap gap-4">
-        {storesList.results.map((store) => (
-          <div className="border w-80">
-            <p>ID: {store.id}</p>
-            <p>{store.name}</p>
+    <div className="flex flex-wrap gap-4 text-white">
+      {storesList.results.map((store, idx) => (
+        <div className="flex flex-row items-center gap-x-4 border rounded-lg w-80 p-2">
+          {/* COMPANY ICON */}
+          <p>{iconList[idx]}</p>
+
+          {/* TEXT CONTAINER */}
+          <div className="flex-grow">
+            <p className="text-xl font-semibold">
+              {store.name} - ID: {store.id}
+            </p>
             <Link href={`https://${store.domain}`} className="underline" target="_blank">
-              {store.domain}
+              Go to {store.name}
             </Link>
-            {/* <p>{store.slug}</p> */}
-            <Image src={store.image_background} alt="Game Company Logo" width={200} height={200} />
-            <p>Over {store.games_count} games available!</p>
+            <p>
+              <span className="font-semibold">{store.games_count}+</span> games available!
+            </p>
           </div>
-        ))}
-      </div>
-      <div className="flex">
-        <p>STORE ICONS</p>
-        <p>
-          Xbox <FaXbox size={50} />
-        </p>
-        <p>
-          Playstation <FaPlaystation size={50} />
-        </p>
-        <p>
-          Steam <FaSteam size={50} />
-        </p>
-        <p>
-          Nintendo <SiNintendo size={50} />
-        </p>
-        <p>
-          Google Play <FaGooglePlay size={50} />
-        </p>
-        <p>
-          Apple <FaApple size={50} />
-        </p>
-        <p>
-          Epic Games <SiEpicgames size={50} />
-        </p>
-        <p>
-          Itch.io <SiItchdotio size={50} />
-        </p>
-        <p>
-          Gog.com <SiGogdotcom size={50} />
-        </p>
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
