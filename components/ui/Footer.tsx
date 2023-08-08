80c889ac885be7e4890207990e9e045dd61b924d
bartswierz <bartoszswierzynski7@gmail.com>
2023-08-08 01:57:39 -0500

    updated footer for better layout responsiveness for small mobile screens

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center  text-white gap-x-1 p-4">
-      Data provided by
-      <Link href="https://rawg.io/apidocs" target="_blank" className="underline">
-        RAWG API
-      </Link>
+    <footer className="flex flex-col sm:flex-row sm:justify-center items-center  text-white gap-x-1 p-4">
+      <div>
+        Data provided by{" "}
+        <Link href="https://rawg.io/apidocs" target="_blank" className="underline">
+          RAWG API
+        </Link>
+      </div>
       <span> - Over 850,000+ Games!</span>
     </footer>
   );
