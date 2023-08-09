ac672d888ea6713acc466ecd02e5c0c1343ee771
bartswierz <bartoszswierzynski7@gmail.com>
2023-08-08 23:01:36 -0500

    update stores page

--- app/browse/stores/page.tsx
+++ app/browse/stores/page.tsx
@@ -26,28 +26,33 @@ const Stores = async () => {
   if (!storesList) return <div className="text-white">Loading...</div>;
 
   return (
-    <div className="flex flex-wrap gap-4 text-white">
+    <div className="flex justify-center flex-wrap gap-4">
       {storesList.results.map((store, idx) => (
-        <div className="flex flex-row items-center gap-x-4 border rounded-lg w-80 p-2">
+        <Link
+          href={`https://${store.domain}`}
+          className="flex flex-col sm:flex-row justify-center sm:justify-start gap-2 text-white items-center rounded-lg w-80 max-w-[86vw] p-2 bg-slate-700 hover:bg-gray-500 text-center sm:text-start sm:gap-4"
+          target="_blank"
+        >
           {/* COMPANY ICON */}
           <p>{iconList[idx]}</p>
 
           {/* TEXT CONTAINER */}
-          <div className="flex-grow">
-            <p className="text-xl font-semibold">
-              {store.name} - ID: {store.id}
-            </p>
-            <Link href={`https://${store.domain}`} className="underline hover:text-blue-500" target="_blank">
-              Go to {store.name}
-            </Link>
+          <div className="text-center sm:text-start">
+            <p className="text-xl font-semibold">{store.name}</p>
+
             <p>
               <span className="font-semibold">{store.games_count}+</span> games available!
             </p>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
 };
 
 export default Stores;
+{
+  /* <Link href={`https://${store.domain}`} className="underline hover:text-blue-500" target="_blank">
+              Go to {store.name}
+            </Link> */
+}
