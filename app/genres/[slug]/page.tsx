07e871f526000d82ace6dc79f4098d160a74c1c5
Bart <bartoszswierzynski7@gmail.com>
2023-08-12 05:49:15 -0500

    updated genres/[slug] page from client to server component for improved load time

--- app/genres/[slug]/page.tsx
+++ app/genres/[slug]/page.tsx
@@ -1,48 +1,21 @@
-"use client";
-import { useSearchParams } from "next/navigation";
 import { getGamesByGenre } from "@/utils";
-import { GamesByGenre } from "@/types";
-import { useState, useEffect } from "react";
 import { GameLink, LoadMoreGenreGames } from "@/components/ui";
-import Loading from "./loading";
-//ex. http://localhost:3000/genres/shooter?genres=action&page_size=6
-//ex. http://localhost:3000/genres/shooter?genres=4&page_size=6
-const GenrePage = async () => {
-  // Grabs the ID from the URL
-  const searchParams = useSearchParams();
-  const searchGenreID = searchParams.get("genres"); // genres=action
-  const [content, setContent] = useState<GamesByGenre>();
 
-  // Fetch Genre Info using the passed ID from browse/genres Page
-  useEffect(() => {
-    //ex searchID for action games is 'action' | 4
-    const fetchGamesByGenre = async (searchID: string | number) => {
-      const data: GamesByGenre = await getGamesByGenre(searchID);
-
-      if (data) {
-        setContent(data);
-      } else throw new Error("No data returned from getGenreInfo");
-    };
-
-    if (searchGenreID) fetchGamesByGenre(searchGenreID);
-  }, [searchGenreID]);
-
-  // const data: GamesByGenre = await getGamesByGenre(searchGenreID);
-
-  // While loading data from API
-  if (!content) return <Loading />;
+//ex. http://localhost:3000/genres/sports?genres=sports
+const GenrePage = async ({ params }: { params: { slug: string } }) => {
+  const content = await getGamesByGenre(params.slug);
 
   return (
     <div className="text-white mx-4 xsm:mx-0">
       <div className="text-4xl font-semibold uppercase flex flex-wrap mb-8">
-        {searchGenreID} GAMES
+        {params.slug} GAMES
         <span className="text-base text-gray-500 ml-2 items-end self-end justify-end">{content.count}+ Games</span>
       </div>
 
       {content && (
         <div className="grid grid-cols-1 px-2 sm:px-0 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-auto gap-4 ">
           {content.results.map(({ slug, name, background_image }) => (
-            // Creates a Game Display for each game in the list as a link to the game page
+            // Creates a Game Card Link for each game, links user to the Games page -> /games/[slug]
             <div key={slug} className="h-64">
               <GameLink slug={slug} name={name} background_image={background_image} />
             </div>
@@ -51,56 +24,9 @@ const GenrePage = async () => {
       )}
 
       {/* LOADING ICON AT THE BOTTOM */}
-      {searchGenreID && <LoadMoreGenreGames searchQuery={searchGenreID} />}
+      {params.slug && <LoadMoreGenreGames searchQuery={params.slug} />}
     </div>
   );
 };
 
 export default GenrePage;
-
-// const GenrePage = async () => {
-//   // Grabs the ID from the URL
-//   const searchParams = useSearchParams();
-//   const searchGenreID = searchParams.get("genres"); // genres=action
-//   const [content, setContent] = useState<GamesByGenre>();
-
-//   // Fetch Genre Info using the passed ID from browse/genres Page
-//   useEffect(() => {
-//     //ex searchID for action games is 'action' | 4
-//     const fetchGamesByGenre = async (searchID: string | number) => {
-//       const data: GamesByGenre = await getGamesByGenre(searchID);
-
-//       if (data) {
-//         setContent(data);
-//       } else throw new Error("No data returned from getGenreInfo");
-//     };
-
-//     if (searchGenreID) fetchGamesByGenre(searchGenreID);
-//   }, [searchGenreID]);
-
-//   // While loading data from API
-//   if (!content) return <Loading />;
-
-//   return (
-//     <div className="text-white mx-4 xsm:mx-0">
-//       <div className="text-4xl font-semibold uppercase flex flex-wrap mb-8">
-//         {searchGenreID} GAMES
-//         <span className="text-base text-gray-500 ml-2 items-end self-end justify-end">{content.count}+ Games</span>
-//       </div>
-
-//       {content && (
-//         <div className="grid grid-cols-1 px-2 sm:px-0 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-auto gap-4 ">
-//           {content.results.map(({ slug, name, background_image }) => (
-//             // Creates a Game Display for each game in the list as a link to the game page
-//             <div key={slug} className="h-64">
-//               <GameLink slug={slug} name={name} background_image={background_image} />
-//             </div>
-//           ))}
-//         </div>
-//       )}
-
-//       {/* LOADING ICON AT THE BOTTOM */}
-//       {searchGenreID && <LoadMoreGenreGames searchQuery={searchGenreID} />}
-//     </div>
-//   );
-// };
