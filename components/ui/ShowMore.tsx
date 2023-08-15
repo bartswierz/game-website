91854eb573f626c908952e2dfdbccd7e26a465d8
bartswierz <bartoszswierzynski7@gmail.com>
2023-08-14 22:38:48 -0500

    updated ShowMore component for improved description display within Games Page

--- components/ui/ShowMore.tsx
+++ components/ui/ShowMore.tsx
@@ -10,26 +10,61 @@ const ShowMore = ({ text }: ShowMoreProps) => {
   const [showText, setShowText] = useState(false);
 
   //REMOVES TAGS AND OTHER UNWANTED TEXT FROM DESCRIPTION
-  // List because some descriptions fetched from API may have multiple paragraphs
-  //TODO - check to ensure it is a string and not an array
-  const formattedTextList: string[] = formatDescription(text);
-  console.log("formattedTextList", formattedTextList);
-  console.log("formattedTextList", typeof formattedTextList);
+  const formattedParagraphList: string[] = formatDescription(text);
+  formattedParagraphList;
 
-  //TODO - some games dont have a long first paragraph, need to extend it to 350 characters if it is less than that
+  interface ConcatenateStringsProps {
+    paragraphList: string[];
+    limit: number;
+  }
+
+  const ShowAll = ({ paragraphList }: { paragraphList: string[] }) => {
+    return (
+      <div className="flex flex-col gap-y-2">
+        {paragraphList.map((paragraph, idx: Key) => (
+          <p key={idx}>{paragraph}</p>
+        ))}
+      </div>
+    );
+  };
+
+  const ShowLess = ({ paragraphList, limit }: ConcatenateStringsProps) => {
+    let currCount = 0;
+    let result: string[] = [];
+
+    for (const paragraph of paragraphList) {
+      // IF THE STRING IS LESS THAN LIMIT OF CHARACTERS, INCREMENT count and push paragraph to result
+      if (result.length + paragraph.length <= limit) {
+        // ADD EACH STRING ARRAY TO RESULT
+        currCount += paragraph.length;
+        result.push(paragraph);
+      } else {
+        //IF THE STRING IS GREATER THAN 350, SLICE IT AT THE REMAINING CHARACTERS LEFT AND PUSH IT TO THE ARRAY LIST
+        const remainingChars = limit - currCount;
+        const slicedParagraph = `${paragraph.slice(0, remainingChars) + "..."}`; //uses up remaining character count to collect last bit of text to
+        result.push(slicedParagraph);
+        break;
+      }
+    }
+
+    return (
+      <div className="flex flex-col gap-y-2">
+        {result.map((paragraph, idx) => (
+          <p key={idx}>{paragraph}</p>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div>
-      {showText
-        ? //DISPLAYS ALL PARAGRAPHS
-          formattedTextList.map((paragraph, idx: Key) => (
-            <>
-              <p key={idx}>{paragraph}</p>
-              {/* If not the last text in the list, add break tag */}
-              {idx !== formattedTextList.length - 1 && <br />}
-            </>
-          ))
-        : //DISPLAYS UP TO 350 Characters
-          `${formattedTextList[0].slice(0, 350)}...`}
+      {showText ? (
+        //DISPLAYS ALL PARAGRAPHS
+        <ShowAll paragraphList={formattedParagraphList} />
+      ) : (
+        //DISPLAYS LIMITED CHARACTERS
+        <ShowLess paragraphList={formattedParagraphList} limit={350} />
+      )}
       <button className="bg-gray-200 text-gray-700 text-xxs rounded p-1 hover:bg-gray-300" onClick={() => setShowText(!showText)}>
         {showText ? "Show less" : "Show more"}
       </button>
