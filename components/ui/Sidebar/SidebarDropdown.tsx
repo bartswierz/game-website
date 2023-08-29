d2df3891e0c49158269a669ac841002ad7f45b65
bartswierz <bartoszswierzynski7@gmail.com>
2023-08-29 04:14:30 -0500

    updated SidebarDropdown with ShadCN accordion components for a smooth transition

--- components/ui/Sidebar/SidebarDropdown.tsx
+++ components/ui/Sidebar/SidebarDropdown.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { useState } from "react";
+import { Accordion, AccordionContent, AccordionTrigger } from "../accordion";
+import { AccordionItem } from "@radix-ui/react-accordion";
 
 interface SidebarDropdownProps {
   pathname: string;
@@ -35,45 +37,80 @@ const SidebarDropdown = ({ pathname, linkTitle, linkList, uid, handleLinkCallBac
   if (!uid) return <div>Loading...</div>;
 
   return (
-    <li>
-      <button
-        type="button"
-        className="flex items-center w-full p-2 text-base text-white animate-in animate-out duration-75 rounded-lg group hover:bg-gray-800 transition-[height]- "
-        aria-controls={`dropdown-example-${uid}`}
-        data-collapse-toggle={`dropdown-example-${uid}`}
-        onClick={() => handleDropdown(uid)}
-      >
-        <IoGameControllerOutline />
-        <span className="flex-1 ml-3 text-left whitespace-nowrap">{linkTitle}</span>
-        {/* DROPDOWN ICON */}
-        <div className={`transition-all duration-300 ${isActive ? "rotate-180" : "rotate-0"}`}>
-          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-          </svg>
-        </div>
-      </button>
-
-      {/* 2ND LEVEL DROPDOWN ITEM */}
-      <ul
-        id={`dropdown-example-${uid}`}
-        className={`transition-all overflow-hidden ease-in-out- animate-accordion-down- duration-300 ${
-          uid === isActive ? "max-h-full- max-h-[height] animate-accordion-up- animate-accordion-down" : "max-h-0 animate-accordion-up"
-        } `}
-      >
-        {linkList.map(({ link, platformID }) => (
-          <li key={platformID}>
-            <Link
-              href={{ pathname: `/${pathname}/${slugify(link)}`, query: { id: platformID } }}
-              onClick={() => handleLink(link)}
-              className="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group hover:bg-gray-800 "
-            >
-              {link}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </li>
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        {/* TEXT GOES HERE */}
+        <AccordionTrigger className="flex items-center justify-between gap-x-3 w-full p-2 text-base text-white animate-in animate-out duration-75 rounded-lg group hover:bg-gray-800">
+          <span className="rotate-0">
+            <IoGameControllerOutline />
+          </span>
+          <span className="text-white w-full text-start">{linkTitle}</span>
+        </AccordionTrigger>
+        <AccordionContent>
+          {linkList.map(({ link, platformID }) => (
+            <li key={platformID}>
+              <Link
+                href={{ pathname: `/${pathname}/${slugify(link)}`, query: { id: platformID } }}
+                onClick={() => handleLink(link)}
+                className="flex items-center w-full p-2 text-white rounded-lg pl-11 hover:bg-gray-800"
+              >
+                {link}
+              </Link>
+            </li>
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
 export default SidebarDropdown;
+
+// return (
+//   <li>
+//     <Accordion type="single" collapsible>
+//       <AccordionItem value="item-1">
+//         <AccordionTrigger>Is it accessible?</AccordionTrigger>
+//         <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+//         <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+//         <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+//       </AccordionItem>
+//     </Accordion>
+//     <button
+//       type="button"
+//       className="flex items-center w-full p-2 text-base text-white animate-in animate-out duration-75 rounded-lg group hover:bg-gray-800 transition-[height]- "
+//       aria-controls={`dropdown-example-${uid}`}
+//       data-collapse-toggle={`dropdown-example-${uid}`}
+//       onClick={() => handleDropdown(uid)}
+//     >
+//       <IoGameControllerOutline />
+//       <span className="flex-1 ml-3 text-left whitespace-nowrap">{linkTitle}</span>
+//       {/* DROPDOWN ICON */}
+//       <div className={`transition-all duration-300 ${isActive ? "rotate-180" : "rotate-0"}`}>
+//         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+//           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+//         </svg>
+//       </div>
+//     </button>
+
+//     {/* 2ND LEVEL DROPDOWN ITEM */}
+//     <ul
+//       id={`dropdown-example-${uid}`}
+//       className={`transition-all overflow-hidden ease-in-out- animate-accordion-down- duration-300 ${
+//         uid === isActive ? "max-h-full- max-h-[height] animate-accordion-up- animate-accordion-down" : "max-h-0 animate-accordion-up"
+//       } `}
+//     >
+//       {linkList.map(({ link, platformID }) => (
+//         <li key={platformID}>
+//           <Link
+//             href={{ pathname: `/${pathname}/${slugify(link)}`, query: { id: platformID } }}
+//             onClick={() => handleLink(link)}
+//             className="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group hover:bg-gray-800 "
+//           >
+//             {link}
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   </li>
+// );
