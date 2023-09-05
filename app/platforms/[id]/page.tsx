db4d4753c7cd18fb1f6fc992ec07a103885e56a7
bartswierz <bartoszswierzynski7@gmail.com>
2023-09-05 12:10:17 -0500

    removed content conditional from platforms/[id]

--- app/platforms/[id]/page.tsx
+++ app/platforms/[id]/page.tsx
@@ -62,58 +62,54 @@ const PlatformPage = () => {
 
   return (
     <div className="text-white">
-      {content && (
-        <>
-          <PageHeader title="Platform Games" count={content.count} countType="Games" />
+      <PageHeader title="Platform Games" count={content.count} countType="Games" />
 
-          <div className="ml-4 sm:ml-0 mb-6 flex justify-center xsm:justify-start">
-            <ComboboxOrdering path={`/platforms/${pageTitle}?id=${searchParamsID}`} page="platforms" />
-          </div>
+      <div className="ml-4 sm:ml-0 mb-6 flex justify-center xsm:justify-start">
+        <ComboboxOrdering path={`/platforms/${pageTitle}?id=${searchParamsID}`} page="platforms" />
+      </div>
 
-          {/* http://localhost:3000/platforms/pc%3Fid=4?ordering=name */}
-          <div>
-            <div className="grid grid-cols-1 px-4 sm:px-0 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 justify-center">
-              {content.results.map(({ slug, background_image, name, stores }) => (
-                <div
-                  key={slug}
-                  className="flex flex-col max-w-[500px] rounded-xl  drop-shadow-md h-max shadow-2xl bg-gray-800 hover:ring-4 hover:ring-primary transition duration-300 ease-in-out"
+      {/* http://localhost:3000/platforms/pc%3Fid=4?ordering=name */}
+      <div>
+        <div className="grid grid-cols-1 px-4 sm:px-0 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 justify-center">
+          {content.results.map(({ slug, background_image, name, stores }) => (
+            <div
+              key={slug}
+              className="flex flex-col max-w-[500px] rounded-xl  drop-shadow-md h-max shadow-2xl bg-gray-800 hover:ring-4 hover:ring-primary transition duration-300 ease-in-out"
+            >
+              {/* GAME IMAGE */}
+              <div className="h-48">
+                {background_image ? (
+                  <Image
+                    src={background_image}
+                    width={200}
+                    height={200}
+                    alt="Game Cover Display"
+                    className="w-full h-full object-cover rounded-t-md "
+                  />
+                ) : (
+                  // IF NO IMAGE URL, DISPLAY BROKEN IMAGE ICON
+                  <div className="flex justify-center items-center h-full">
+                    <MdBrokenImage size={80} color="grey" />
+                  </div>
+                )}
+              </div>
+
+              {/* TEXT CONTAINER */}
+              <div className="text-center p-4">
+                <h2 className="text-xl mb-3 font-bold truncate">{name}</h2>
+
+                {/* NAVIGATES USER TO GAMES PAGE -> i.e.) http://localhost:3000/games/marvels-spider-man */}
+                <Link
+                  href={`/games/${slug}`}
+                  className="flex justify-center items-center font-semibold rounded-full hover:bg-gray-400  bg-gray-300 text-gray-700 px-2 py-1 max-w-1/2 text-base xsm:text-lg "
                 >
-                  {/* GAME IMAGE */}
-                  <div className="h-48">
-                    {background_image ? (
-                      <Image
-                        src={background_image}
-                        width={200}
-                        height={200}
-                        alt="Game Cover Display"
-                        className="w-full h-full object-cover rounded-t-md "
-                      />
-                    ) : (
-                      // IF NO IMAGE URL, DISPLAY BROKEN IMAGE ICON
-                      <div className="flex justify-center items-center h-full">
-                        <MdBrokenImage size={80} color="grey" />
-                      </div>
-                    )}
-                  </div>
-
-                  {/* TEXT CONTAINER */}
-                  <div className="text-center p-4">
-                    <h2 className="text-xl mb-3 font-bold truncate">{name}</h2>
-
-                    {/* NAVIGATES USER TO GAMES PAGE -> i.e.) http://localhost:3000/games/marvels-spider-man */}
-                    <Link
-                      href={`/games/${slug}`}
-                      className="flex justify-center items-center font-semibold rounded-full hover:bg-gray-400  bg-gray-300 text-gray-700 px-2 py-1 max-w-1/2 text-base xsm:text-lg "
-                    >
-                      <span className="truncate">See Game Details</span>
-                    </Link>
-                  </div>
-                </div>
-              ))}
+                  <span className="truncate">See Game Details</span>
+                </Link>
+              </div>
             </div>
-          </div>
-        </>
-      )}
+          ))}
+        </div>
+      </div>
 
       {/* {content?.next searchID && <LoadMorePlatformGames searchQuery={searchID} />} */}
       {searchParamsID && <LoadMorePlatformGames searchQuery={searchParamsID} />}
