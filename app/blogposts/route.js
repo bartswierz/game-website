355a636c60a690232151c2e3066101a77347b238
bartswierz <bartoszswierzynski7@gmail.com>
2023-11-10 04:08:10 -0600

    test api routes added for testing purposes

---
+++ app/blogposts/route.js
@@ -0,0 +1,20 @@
+// Testing ROUTER HANDLERS - Note: WORKS AS LONG AS THERES NO 'page.js' in this route
+//http://localhost:3000/blogposts - See in Browser or postman
+export async function GET() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  return Response.json({ posts });
+  // return console.log("hello from server"), Response.json({ posts });
+}
+
+export async function POST(request) {
+  // posting a blog post
+
+  return Response.json({ msg: "Hello from POST /blogposts" });
+}
+
+export async function DELETE(request) {
+  // deleting a blog post
+  return Response.json({ msg: "Hello from DELETE /blogposts" });
+}
