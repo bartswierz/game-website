47593337e0bab3a3053603a0717f03bf614b0c4f
Bart <bartoszswierzynski7@gmail.com>
2023-11-14 04:13:54 -0600

    updated signout button design in navbar and updated router handler logic to return user back to localhost:3000/login

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -5,10 +5,17 @@ import { useAppSelector } from "@/redux/store";
 import Link from "next/link";
 import { signOut } from "@/auth";
 import { BiLogOut } from "react-icons/bi";
+import { useRouter } from "next/navigation";
 //BUILT CUSTOM GAME ICON - https://game-icons.net/1x1/caro-asercion/warlord-helmet.html
 const Navbar = () => {
   // Get the isSidebarOpen value from redux store to hide hamburger menu when sidebar is open
   const isSidebarOpen: boolean = useAppSelector(({ sidebarSlice }) => sidebarSlice.value.isSidebarOpen);
+  const router = useRouter(); //will be used to on SIGNOUT to give a clean url back to /login without the added callback url params
+
+  const handleSignOut = async () => {
+    await signOut({ redirect: false });
+    router.push("/login"); //cleans up our route to localhost:3000/login
+  };
 
   return (
     <header>
@@ -17,19 +24,9 @@ const Navbar = () => {
         <Searchbar />
         {/* Hides menu icon when sidebar is opened */}
         {isSidebarOpen ? <div className="w-10 h-10"></div> : <HamburgerMenu />}
-        {/* <Link href={"/login"}>Sign In</Link> */}
-        {/* <Link href={"/login"} className="hidden md:block">
-          Sign In
-        </Link> */}
-        <form
-          // action={async () => signOut()}
-          action={async () => {
-            // "use server";
-            await signOut();
-          }}
-        >
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50- p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            {/* <BiLogOut size={24} /> */}
+        <form action={handleSignOut}>
+          <button className="flex gap-2 bg-blue-600 px-4 py-2 rounded-full hover:bg-blue-700 focus:bg-blue-800 transition-colors duration-300 shadow-lg">
+            <BiLogOut size={24} />
             Sign Out
           </button>
         </form>
