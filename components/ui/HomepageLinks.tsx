8c52e6f8b6c26e695a5d76569800338095edf96e
Bart <bartoszswierzynski7@gmail.com>
2023-11-24 23:33:37 -0600

    updated design of our Homepage Links

--- components/ui/HomepageLinks.tsx
+++ components/ui/HomepageLinks.tsx
@@ -21,22 +21,22 @@ const HomepageLinks = () => {
     return (
       <Link
         href={`${hrefText}`}
-        className="relative flex flex-col justify-center items-center bg-gradient-to-b from-slate-900 to-blue-700 font-semibold rounded-lg text-2xl sm:text-3xl hover:to-blue-500 group hover:bg-blue-700 w-full h-full overflow-hidden z-[1]"
+        className="relative flex flex-col justify-center items-center bg-gradient-to-b from-blue-900  to-slate-800 font-semibold rounded-lg text-2xl sm:text-3xl hover:to-gray-500 group hover:bg-gray-700 w-full h-full overflow-hidden z-[1] min-h-full drop-shadow-lg"
       >
         <Image
           src={imagePath}
           alt="Category Link"
           fill
-          className="absolute object-cover object-center w-full- h-full-  z-[-1] opacity-50 group-hover:opacity-100"
+          className="absolute object-cover object-center z-[-1] opacity-30 group-hover:opacity-80 transition-all ease-in duration-200"
         />
         {linkText}
-        <span className="text-base font-medium text-gray-500 group-hover:text-white group-hover:font-bold">{secondaryText}</span>
+        <span className="text-base font-medium text-gray-400 group-hover:text-white group-hover:font-bold">{secondaryText}</span>
       </Link>
     );
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-[70vh] p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-[70vh] p-4 mb-[10vh]-">
       <LinkCard hrefText="/dashboard/browse/genres" linkText="Genres" secondaryText="19 Genres" imagePath={GenresImage} />
       <LinkCard
         hrefText="/dashboard/browse/developers"
