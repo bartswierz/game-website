5635f8ca16e18479bc822560c2bb42d9494c7bad
Bart <bartoszswierzynski7@gmail.com>
2023-11-24 22:23:12 -0600

    fixed authorization routing logic to resolve infinite loop to dashboard

--- auth.config.ts
+++ auth.config.ts
@@ -24,44 +24,25 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       // console.log("request { nextUrl }: ", nextUrl);
       const isLoggedIn = !!auth?.user;
-      console.log("auth.user: ", auth.user);
-      // console.log("isLoggedIn: ", isLoggedIn);
-      // if (isLoggedIn) return true;
-      // console.log("isLoggedIn: ", isLoggedIn);
-      // console.log("auth: ", auth);
+      console.log("auth.user: ", auth?.user);
+
       const isOnAllowedPage = nextUrl.pathname === "/login" || nextUrl.pathname === "/signup";
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard"); //may have to update dashboard path
-      // console.log("nextUrl: ", nextUrl);
-      // console.log("nextUrl.pathname: ", nextUrl.pathname);
-      // const isOnDashboard = nextUrl.pathname.startsWith("/"); //may have to update dashboard path
 
-      //TODO - issue here with too many redirects to /dashboard page when still trying to create account or login. Need to fix this before updating main branch
-      //Prevents user from accessing the dashboard pages without being logged in(ex. user trying to change the url manually to bypass authorization example.com/dashboard
-      // THIS ROUTE IS CORRECTLY NAVIGATING USER
       if (isOnDashboard && !isLoggedIn) {
-        // Redirect unauthenticated users trying to access dashboard pages to login page
-        console.log("#1 - isOnDashboard && !isLoggedIn - BUT NOT AUTHENTICATED, REDIRECTING TO /login");
-        // return false;
+        // Redirect unauthenticated users trying to access dashboard pages to login page(ex. modifying the url to http://localhost:3000/dashboard will redirect to http://localhost:3000/login)
         return Response.redirect(new URL("/login", nextUrl));
+      } else if (isLoggedIn && !isOnDashboard) {
+        // Redirect authenticated users to the dashboard only if they are not already there - (ex. user has created an account OR user has logged in, and they are trying to access the login page, so we redirect them to the dashboard page)
+        return Response.redirect(new URL("/dashboard", nextUrl));
       } else if (isOnAllowedPage) {
-        console.log("#2 - isOnAllowedPage");
-        // Always allow access to login and signup pages
+        // Always allow access to 'login' and 'signup' pages
         return true;
-        // return false;
-      } else if (isLoggedIn) {
-        //TODO - problem is in here
-        // Redirect authenticated users to the dashboard
-        console.log("#3 - isLoggedIn - PROBLEM IS HERE REDIRECTING TOO MANY TIMES TO /dashboard");
-        // return Response.redirect(new URL("/dashboard", nextUrl));
-        // return true;
-      } else if (!isLoggedIn && !isOnDashboard) {
-        // return Response.redirect(new URL("/login", nextUrl));
-        console.log("#4 - !isLoggedIn && !isOnDashboard - Stay on current page (login or signup page)");
-        // return false;
-        return Response.redirect(new URL("/login", nextUrl)); //redirect to login page to get authorized to access dashboard
+      } else if (!isLoggedIn) {
+        // Redirect unauthenticated users not trying to access dashboard to login page
+        return Response.redirect(new URL("/login", nextUrl));
       }
 
-      console.log("outside of if statements, returning true...");
       // For all other cases, return true to allow access
       return true;
     },
