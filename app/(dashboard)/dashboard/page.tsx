14c733e2bc865d0bc651e17ea3971239d0596de8
Bart <bartoszswierzynski7@gmail.com>
2023-11-24 23:13:44 -0600

    added our custom category link images

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
     // <main className="m-24 rounded-md grid grid-cols-4 gap-12 w-full">
     <main className="text-white">
       <h1 className="text-center text-3xl sm:text-4xl font-bold mb-[4vh]">Where Classics and New Games Collide</h1>
+      {/* Contains 4 Container Links: Genres, Developers, Platforms, Stores */}
       <HomepageLinks />
     </main>
   );
